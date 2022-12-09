8e83a0b638725a91b14fffaac35165e05ef92e66
Yahia <yahyouch36@gmail.com>
2022-12-09 14:24:16 +0100

    contact meds

--- src/Components/FAQ.jsx
+++ src/Components/FAQ.jsx
@@ -1,41 +1,94 @@
-import React from 'react'
+import React from "react";
 
 const FAQ = () => {
   return (
     <div>
-        <div class="text-darkish text-[60px] font-display text-center mx-auto mt-10">
+      <div class="text-darkish text-[60px] font-display text-center mx-auto mt-10">
         FAQ
-        </div>
-        <div class="font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
-        <h1 class="text-[40px]">
-        How can we participate ?
-        </h1>
+      </div>
+      <div class="font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
+        <h1 class="text-[40px]">How can we participate ?</h1>
         <p class="text-[19px]">
-        You find the link to the registrations either on our Instagram
-        bio or in a pinned post on our facebook page.
+          You find the link to the registrations either on our Instagram bio or
+          in a pinned post on our facebook page.
         </p>
+      </div>
+      <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
+        <p class="text-[25px]">Do I need to have a team before joining?</p>
+      </div>
+      <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
+        <p class="text-[25px]">Why join us ?</p>
+      </div>
+      <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
+        <p class="text-[25px]">Is it free? </p>
+      </div>
+      <div class="font-display text-white mt-5 mb-2 rounded-[500px] mx-auto bg-lightPurple h-[400px] w-[1100px]">
+        <h1 class="align-center ml-[400px] text-[60px]">CONTACT US</h1>
+        <div class="ml-[200px]">
+          <img
+            class="ml-[300px] w-[80px] h-[120px]"
+            src="https://i.ibb.co/T1Q3WFK/cse.png"
+            alt="cse"
+            border="0"
+          ></img>
         </div>
-        <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
-        <p class="text-[25px]">
-        Do I need to have a team before joining?
-         </p>
+        <div class ="ml-[100px] mt-10">
+        <div class="flex space-x-20">
+          <div class="ml-[200px]">
+            <a href="https://imgbb.com/">
+              <img
+                class="w-[50px] h-[50px]"
+                src="https://i.ibb.co/hLqnq3b/Twitter.png"
+                alt="Twitter"
+                border="0"
+              />
+            </a>
+          </div>
+          <div class="ml-[200px]">
+            <a href="https://imgbb.com/">
+              <img
+                class="w-[50px] h-[50px]"
+                src="https://i.ibb.co/D8xFHGq/instagram.png"
+                alt="instagram"
+                border="0"
+              />
+            </a>
+          </div>
+          <div class="ml-[200px]">
+            <a href="https://imgbb.com/">
+              <img
+                class="w-[50px] h-[50px]"
+                src="https://i.ibb.co/Kxj24sB/youtube.png"
+                alt="youtube"
+                border="0"
+              />
+            </a>
+          </div>
+          <div class="ml-[200px]">
+            <a href="https://imgbb.com/">
+              <img
+                class="w-[50px] h-[50px]"
+                src="https://i.ibb.co/Z8SgZG9/facebook.png"
+                alt="facebook"
+                border="0"
+              ></img>
+            </a>
+          </div>
+          <div class="ml-[200px]">
+            <a href="https://imgbb.com/">
+              <img
+                class="w-[50px] h-[50px]"
+                src="https://i.ibb.co/PFdwQtF/linkedin.png"
+                alt="linkedin"
+                border="0"
+              ></img>
+            </a>
+          </div>
         </div>
-        <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
-        <p class="text-[25px]">
-        Why join us ?</p>
         </div>
-        <div class="mt-5 font-display p-10 mx-auto container justify-center align-center text-white bg-darkish">
-        <p class="text-[25px]">
-        Is it free? </p>
-        </div>
-        <div class="font-display text-white mt-5 mb-2 rounded-[500px] mx-auto bg-lightPurple h-[400px] w-[1100px]">
-            <h1 class="align-center ml-[400px] text-[60px]">CONTACT US</h1>
-           <div class="ml-[200px]">
-           <img class="w-[80px] h-[120px]"src="https://i.ibb.co/T1Q3WFK/cse.png" alt="cse" border="0"></img>
-         </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
