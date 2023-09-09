efb58e2e18a0d6ec74f5a941992e078ee0357ab4
Yahia Abdeldjalil <ly_benyahia@esi.dz>
2023-09-09 21:46:54 +0100

    fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,27 +7,27 @@ const Navbar = () => {
       <nav className="shadow-lg mb-2 bg-gradient-to-bl from-primary via-secondary to-tertiary text-white font-display py-4 px-10 flex justify-between">
         <img src={logo} alt="logo" className="w-14 h-14" />
         <ul className="mt-2 flex justify-between space-x-4 text-2xl">
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/about">About</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/us">Us</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/agenda">Agenda</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/speakers">Speakers</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/faq">FAQ</Link>
-          </li>
-          <li className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
-            <Link to="/faq">Contact</Link>
-          </li>
+        <Link to="/" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            Home
+          </Link>
+          <Link to="/about" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            About
+          </Link>
+          <Link to="/us" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            Us
+          </Link>
+          <Link to="/agenda" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            Agenda
+          </Link>
+          <Link to="/speakers" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            Speakers
+          </Link>
+          <Link to="/faq" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            FAQ
+          </Link>
+          <Link to="/faq" className="hover:cursor-pointer border-[1px] border-transparent hover:border-white rounded-full px-8 py-2 flex">
+            Contact
+          </Link>
         </ul>
       </nav>
     </div>
